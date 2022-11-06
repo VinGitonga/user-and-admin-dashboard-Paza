8d3595301d0704b1f928daf51f1ee99e934ee3bf
Vincent Gitonga <gitongavincent64@gmail.com>
2022-11-06 15:27:17 +0000

    changed the forum item

--- src/pages/Forums.jsx
+++ src/pages/Forums.jsx
@@ -1,89 +1,44 @@
 import React from "react";
 import Header from "../components/Header";
 
+import { BiUpvote, BiDownvote, BiCommentDetail } from "react-icons/bi";
+import "./Forums.css";
+import ForumItem from "../components/forums/ForumItem";
 
+const Forum = () => {
+    const allContent = [
+        {
+            title: "Water shortage",
+            name: "Joan Munde",
+            topic: "shortage of water",
+            description:
+                "water has been an issue, we lack water it is has been one week now",
+        },
+        {
+            title: "Water",
+            name: "Mollen Munde",
+            topic: "shortage of kibandas",
+            description:
+                "water has been an issue, we lack water it is has been one week now",
+        },
+        {
+            title: "Water",
+            name: "Angela Munde",
+            topic: "shortage of electricity",
+            description:
+                "water has been an issue, we lack water it is has been one week now",
+        },
+    ];
 
-
-import { BiUpvote,BiDownvote,BiCommentDetail } from "react-icons/bi";
-import './Forums.css'
-
-const Forum = ()=>{
-  //  const content ={
-  //      title: "Water shortage",
-  //     name:"Joan Munde",
-  //     topic: "shortage of water",
-  //       description: "water has been an issue, we lack water it is has been one week now",
-    
-       
-        
-
-
-
-  //    }
-  //   const content1 ={
-  //     title: "Water",
-  //     name:"Mollen Munde",
-  //    topic: "shortage of kibandas",
-  //    description: "water has been an issue, we lack water it is has been one week now",
-
-
-  //    }
-  //   const content2 ={
-  //     title: "Water",
-  //      name:"Angela Munde",
-  //     topic: "shortage of electricity",
-  //       description: "water has been an issue, we lack water it is has been one week now",
-  //  }
-
-    return(
-        <div className="m-2 md:m-10 p-2 md:p-10 bg-white">
-            <Header title="Forums" category=""/>
-            {/* <ForumsTile content ={content}/>
-            <ForumsTile content ={content1}/>
-            <ForumsTile content ={content2}/> */}
-
-
-
-<div className='box'>
-        <div className='boxes'>
-      <h2>To commemorate World Health Day, Kilimani Children's clinic is offering free dental consultations and 30% discounts on select procedures throughout this ...
-</h2>
+    return (
+        <div className="my-3 px-8">
+            <div className="mx-5">
+                <Header title="Forums" category="" />
+            </div>
+            {allContent.map((item, i) => (
+                <ForumItem data={item} key={i} />
+            ))}
         </div>
-        <div className='icons'>
-            <div id='heart' ><BiUpvote /></div>
-            <div id='down'><BiDownvote /></div>
-            <div id='Comment'><BiCommentDetail /></div>
-          </div>
-      </div>
-      <div className='box4'>
-      </div>
-      <div className='box5'>
-      </div>
-      <div className='box6'>
-      </div>
-      <div className='box7'>
-      </div>
-      <div class="flow-root">
-      </div>
-    </div>
-  )
-}
-export default Forum
-
-          
-
-
-
-
-
-
-
-
-           
-            
-
-
-
-    
-    
-
+    );
+};
+export default Forum;
